d45d8793c727b981a29b98ee52694f566a3f5b29
Helen751 <okerekehelenugoeze@gmail.com>
2026-02-24 20:00:03 +0200

    adding google analytics

--- header.php
+++ header.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BF8JRXWVQD"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-BF8JRXWVQD');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Helen Okereke | Software Engineer, IoT & Tech for Impact</title>
